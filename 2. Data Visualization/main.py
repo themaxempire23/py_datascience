fdd3b843ae0ee3ab14a8a8275ae9cbbf11d58351
max <Phillemon.Haikali@transnamib.com.na>
2026-01-15 23:11:28 +0200

    matplot 1 complete

---
+++ 2. Data Visualization/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = [1,2,3,4,5,6,7,8,9,10]
+y = [10, 15, 7, 12, 8, 7, 5, 6, 8, 9]
+
+plt.plot(x, y)
+plt.title('whk line plot')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.show()
